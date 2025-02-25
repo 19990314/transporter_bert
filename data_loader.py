fda5f872e96b15d0af4ac3ea2ef7541b24654fc8
chen <chenshuting0314@yahoo.com>
2025-02-24 20:16:43 -0800

    plot pipeline finished

--- data_loader.py
+++ data_loader.py
@@ -7,20 +7,41 @@ from transformers import BertModel, BertTokenizer, BertConfig
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Load gene expression matrix
-expr_matrix = pd.read_csv("expression_matrix.csv", index_col=0)
-metadata = pd.read_csv("metadata.csv", index_col=0)
-gene_names = pd.read_csv("gene_names.csv", header=None)[0].tolist()
+class GeneExpressionDataset(Dataset):
+    def __init__(self, adata, mask_ratio=0.15):
+        self.data = torch.tensor(adata.X, dtype=torch.float32)
+        self.mask_ratio = mask_ratio
 
-# Convert data to AnnData format
-adata = sc.AnnData(expr_matrix.T)  # Cells as rows, genes as columns
-adata.obs = metadata  # Assign metadata
-adata.var_names = gene_names
+    def __len__(self):
+        return self.data.shape[0]
 
-# Normalize Data
-sc.pp.normalize_total(adata, target_sum=1e4)
-sc.pp.log1p(adata)
+    def __getitem__(self, idx):
+        x = self.data[idx].clone()
+        mask = torch.rand(x.shape) < self.mask_ratio
+        x[mask] = 0  # Masking genes
+        return x, self.data[idx]  # (Masked input, Original target)
 
-# Select highly variable genes
-sc.pp.highly_variable_genes(adata, flavor="seurat_v5", n_top_genes=2000)
-adata = adata[:, adata.var.highly_variable]
+def load_data(proj_root):
+    # Load gene expression matrix
+    expr_matrix = pd.read_csv(proj_root+"expression_matrix.csv", index_col=0)
+    metadata = pd.read_csv("metadata.csv", index_col=0)
+    gene_names = pd.read_csv("gene_names.csv", header=None)[1].tolist()
+    print("data loaded")
+
+
+    # Convert data to AnnData format
+    adata = sc.AnnData(expr_matrix.T)  # Cells as rows, genes as columns
+    adata.obs = metadata  # Assign metadata
+    adata.var_names = gene_names
+
+    # Normalize Data
+    sc.pp.normalize_total(adata, target_sum=1e4)
+    sc.pp.log1p(adata)
+
+    # Select highly variable genes
+    sc.pp.highly_variable_genes(adata, flavor="seurat", n_top_genes=200)
+    adata = adata[:, adata.var.highly_variable]
+
+    return adata, gene_names
+
+
